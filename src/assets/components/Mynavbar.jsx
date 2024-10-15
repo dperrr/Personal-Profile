774b263e8655e63b86c58e7897e3b197c43abaaf
dperrr <rosellonjasperdan30@gmail.com>
2024-10-16 00:26:17 +0800

    Just intiliaze my project and did some folder structuring

---
+++ src/assets/components/Mynavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Mynavbar() {
+    return (
+        <>
+            <h1>tite</h1>
+
+        </>
+    )
+}
+
+
+export default Mynavbar;
---
+++ src/assets/router/Approuter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function Approuter() {
+    return (
+        <>
+            
+        </>
+    )
+}
