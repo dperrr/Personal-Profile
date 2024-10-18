9cd3d1cd1917b91ab36aa59f0fa68df0c55ad165
dperrr <rosellonjasperdan30@gmail.com>
2024-10-19 01:39:59 +0800

    Last push shitty unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Mynavbar from './assets/components/MyNavbar'
 import Approuter from './assets/router/Approuter'
 
 function App() {
